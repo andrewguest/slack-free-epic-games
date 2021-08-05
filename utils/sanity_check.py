39036f9129d04bc36e078c765c9491a883716547
andrewguest <andrew@aguest.me>
2021-08-05 12:12:49 -0500

    Removed time constraint

--- utils/sanity_check.py
+++ utils/sanity_check.py
@@ -1,18 +1,7 @@
-from datetime import date, datetime
-
-
 def safe_to_post():
-    """Sanity check to verify a few constraints:
-    1. That it's Thursday.
-    2. That it's 3:15pm UTC or later.
-    3. That these games haven't already been posted.
-    """
-    current_utc_time = datetime.utcnow()
-    if date.today().weekday() == 3 and (current_utc_time.hour >= 15 and current_utc_time.minute >= 15):
-        with open("posted_games.txt") as log_file:
-            lines = log_file.readlines()
-            last_two_lines = lines[-2:]
-            last_two_lines = [line.strip() for line in last_two_lines]
-        return last_two_lines
-    else:
-        return False
+    """Sanity check to verify that these games haven't already been posted."""
+    with open("posted_games.txt") as log_file:
+        lines = log_file.readlines()
+        last_two_lines = lines[-2:]
+        last_two_lines = [line.strip() for line in last_two_lines]
+    return last_two_lines
