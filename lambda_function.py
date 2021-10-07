08a3d5d9fcd91d74e21b7cb437fe7fd3987e8f20
andrewguest <andrew@aguest.me>
2021-10-06 20:23:32 -0500

    Formatting for AWS Lambda deployment

--- lambda_function.py
+++ lambda_function.py
@@ -57,52 +57,51 @@ def get_free_epic_games() -> List[Game]:
 
 
 # Slack setup
-slack_client = WebClient(token=os.getenv("SLACK_TOKEN"))
-free_epic_games = get_free_epic_games()
+def lambda_handler(event, context):
+	slack_client = WebClient(token=os.getenv("SLACK_TOKEN"))
+	free_epic_games = get_free_epic_games()
 
-epic_games_list = [game.title for game in free_epic_games]
-
-for entry in free_epic_games:
-    game_blocks = [
-        {
-			"type": "section",
-			"text": {
-				"type": "mrkdwn",
-				"text": f"*{entry.title}*"
+	for entry in free_epic_games:
+		game_blocks = [
+			{
+				"type": "section",
+				"text": {
+					"type": "mrkdwn",
+					"text": f"*{entry.title}*"
+				},
+				"accessory": {
+					"type": "image",
+					"image_url": f"{entry.image_url}",
+					"alt_text": f"{entry.title}"
+				}
 			},
-			"accessory": {
-				"type": "image",
-				"image_url": f"{entry.image_url}",
-				"alt_text": f"{entry.title}"
-			}
-		},
-		{
-			"type": "section",
-            "text": {
-                "type": "plain_text",
-                "text": f"{entry.store_link}"
-            },
-			"accessory": {
-				"type": "button",
+			{
+				"type": "section",
 				"text": {
 					"type": "plain_text",
-					"text": "Epic store link"
+					"text": f"{entry.store_link}"
 				},
-				"value": "click_me_123",
-				"url": f"{entry.store_link}",
-				"action_id": "button-action"
+				"accessory": {
+					"type": "button",
+					"text": {
+						"type": "plain_text",
+						"text": "Epic store link"
+					},
+					"value": "click_me_123",
+					"url": f"{entry.store_link}",
+					"action_id": "button-action"
+				}
+			},
+			{
+				"type": "divider"
 			}
-		},
-		{
-			"type": "divider"
-		}
-    ]
+		]
 
-    try:
-        slack_client.chat_postMessage(
-            channel = str(os.getenv("SLACK_CHANNEL_TOKEN")),  # get this from an env variable
-            blocks = game_blocks
-        )
-    except SlackApiError as e:
-        assert e.response["error"]
+		try:
+			slack_client.chat_postMessage(
+				channel = str(os.getenv("SLACK_CHANNEL_TOKEN")),  # get this from an env variable
+				blocks = game_blocks
+			)
+		except SlackApiError as e:
+			assert e.response["error"]
 
