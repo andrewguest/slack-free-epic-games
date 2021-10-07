a7e9521680084eba3cc23d70efecc549d35e772a
andrewguest <andrew@aguest.me>
2021-10-06 20:13:51 -0500

    Consolidated into one file

--- lambda_function.py
+++ lambda_function.py
@@ -1,10 +1,20 @@
+import os
 from typing import List
 
 import httpx
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
 
-from models.games import Game
+
+# Model setup
+class Game:
+    def __init__(self, title, store_link, image_url) -> None:
+        self.title = title
+        self.store_link = store_link
+        self.image_url = image_url
 
 
+# Epic functionality
 def get_free_epic_games() -> List[Game]:
     """Uses an API from Epic to parse a list of free games to find this week's free games."""
     # HTTP params for the US free games
@@ -44,3 +54,55 @@ def get_free_epic_games() -> List[Game]:
             free_games.append(game)
 
     return free_games
+
+
+# Slack setup
+slack_client = WebClient(token=os.getenv("SLACK_TOKEN"))
+free_epic_games = get_free_epic_games()
+
+epic_games_list = [game.title for game in free_epic_games]
+
+for entry in free_epic_games:
+    game_blocks = [
+        {
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": f"*{entry.title}*"
+			},
+			"accessory": {
+				"type": "image",
+				"image_url": f"{entry.image_url}",
+				"alt_text": f"{entry.title}"
+			}
+		},
+		{
+			"type": "section",
+            "text": {
+                "type": "plain_text",
+                "text": f"{entry.store_link}"
+            },
+			"accessory": {
+				"type": "button",
+				"text": {
+					"type": "plain_text",
+					"text": "Epic store link"
+				},
+				"value": "click_me_123",
+				"url": f"{entry.store_link}",
+				"action_id": "button-action"
+			}
+		},
+		{
+			"type": "divider"
+		}
+    ]
+
+    try:
+        slack_client.chat_postMessage(
+            channel = str(os.getenv("SLACK_CHANNEL_TOKEN")),  # get this from an env variable
+            blocks = game_blocks
+        )
+    except SlackApiError as e:
+        assert e.response["error"]
+
--- models/games.py
+++
@@ -1,5 +0,0 @@
-class Game:
-    def __init__(self, title, store_link, image_url) -> None:
-        self.title = title
-        self.store_link = store_link
-        self.image_url = image_url
